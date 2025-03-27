aeff98550715d5264ffad301a67597d7593906af
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-03-27 23:36:29 +0530

    [Nikhil] Add: added product store.

---
+++ com/bridgelabz/onlineMarketPlace/ProductStore.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.onlineMarketPlace;
+
+import java.util.*;
+
+public class ProductStore {
+    private List<Product<? extends ProductCategory>> products = new ArrayList<>();
+
+    public void addProduct(Product<? extends ProductCategory> product){
+        products.add(product);
+    }
+
+    public void displayDetails(){
+        System.out.println("Product Details:");
+        for(Product<? extends ProductCategory> product : products){
+            product.displayProduct();
+        }
+    }
+
+
+
+}
