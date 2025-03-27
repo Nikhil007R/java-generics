1520f74a618545f54fa505af55a12946c8c7de94
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-03-27 23:42:49 +0530

    [Nikhil] Add: added course

---
+++ com/bridgelabz/courseManagement/Course.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.courseManagement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Generic Course Class
+class Course<T extends CourseType> {
+    private List<T> courseList = new ArrayList<>();
+
+    // Method to add a course
+    public void addCourse(T course) {
+        courseList.add(course);
+    }
+
+    // Method to display courses
+    public void displayCourses() {
+        System.out.println("\nCourses Offered:");
+        for (T course : courseList) {
+            System.out.println(course.getCourseName() + " - Evaluation: " + course.getEvaluationMethod());
+        }
+    }
+}
