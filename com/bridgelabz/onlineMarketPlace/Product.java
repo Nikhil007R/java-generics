e4fe0f28059ed460e347553b6cc2e0244a2449ad
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-03-27 23:20:36 +0530

    [Nikhil] Add: created abstract product class.

---
+++ com/bridgelabz/onlineMarketPlace/Product.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.onlineMarketPlace;
+
+abstract class Product<T> {
+    private String name;
+    private double price;
+    private T category;
+
+    public Product(String name, double price, T category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public T getCategory() {
+        return category;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " - Name: " + name + ", Price: $" + price + ", Category: " + category;
+    }
+}
+
