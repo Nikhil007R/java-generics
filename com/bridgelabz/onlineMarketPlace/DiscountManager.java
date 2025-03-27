838d34ff397c6c240fff1eb6c295ebff4c89acba
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-03-27 23:37:26 +0530

    [Nikhil] Add: DiscountManager added

---
+++ com/bridgelabz/onlineMarketPlace/DiscountManager.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.onlineMarketPlace;
+
+public class DiscountManager {
+    public static <T extends ProductCategory> void applyDiscount(Product<T> product, double percentage){
+        double price = product.getPrice();
+        price = price - (price * percentage / 100);
+        product.setPrice(price);
+    }
+}
